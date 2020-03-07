2305d5829b970844c11c38c0f3184663445b4b13
why-2004 <h1710149@nushigh.edu.sg>
2020-03-07 11:04:24 +0800

    now has every feature

---
+++ app/src/main/java/com/why/lab7/FallService.kt
@@ -0,0 +1,139 @@
+package com.why.lab7
+
+import android.app.Service
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.media.MediaPlayer
+import android.os.Binder
+import android.os.IBinder
+import android.speech.tts.TextToSpeech
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+import kotlin.math.abs
+import kotlin.math.min
+
+
+class FallService: Service(), SensorEventListener {
+    private var mSensorManager : SensorManager ?= null
+
+    private var mAccelerometer : Sensor ?= null
+    private var mLight:Sensor?=null
+    private lateinit var tts:TextToSpeech
+
+    private val mBinder = LocalBinder()
+    protected var mToast: Toast? = null
+    var myService: FallService? = null
+    var isBound = false
+    var lightLastChange:Boolean?=null
+    lateinit var mediaPlayer: MediaPlayer
+
+
+
+    inner class LocalBinder : Binder() {
+        fun getService(): FallService? {
+            return this@FallService
+        }
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return mBinder
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        mLight = mSensorManager!!.getDefaultSensor(Sensor.TYPE_LIGHT)
+
+        tts = TextToSpeech(applicationContext,
+            TextToSpeech.OnInitListener { })
+
+        val l=resources.configuration.locales[0]
+        val a = tts.isLanguageAvailable(l)
+        if ( a== TextToSpeech.LANG_COUNTRY_AVAILABLE || a== TextToSpeech.LANG_COUNTRY_VAR_AVAILABLE || a== TextToSpeech.LANG_AVAILABLE){
+            tts.language = l
+        }else{
+            tts.language= Locale.ENGLISH
+        }
+        mSensorManager!!.registerListener(this, this.mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        mSensorManager!!.registerListener(this, this.mLight, SensorManager.SENSOR_DELAY_NORMAL)
+
+        mediaPlayer= MediaPlayer.create(this, R.raw.scream)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mSensorManager?.unregisterListener(this)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        return START_STICKY
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+    var minA=100000000000.0
+
+    override fun onSensorChanged(sensorEvent: SensorEvent?) {
+
+        val mySensor: Sensor = sensorEvent!!.sensor
+        when (mySensor.type) {
+            Sensor.TYPE_ACCELEROMETER -> {
+
+
+                mAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+                val alpha = 0.8
+
+                var gravity= mutableListOf(0.0,0.0,0.0)
+                gravity[0] = alpha * gravity[0] + (1 - alpha)* sensorEvent.values[0]
+                gravity[1] = alpha * gravity[1] + (1 - alpha) * sensorEvent.values[1]
+                gravity[2] = alpha * gravity[2] + (1 - alpha) * sensorEvent.values[2]
+
+
+                var linearAcceleration=mutableListOf<Double>(0.0,0.0,0.0)
+                linearAcceleration[0] = sensorEvent.values[0] - gravity[0]
+                linearAcceleration[1] = sensorEvent.values[1] - gravity[1]
+                linearAcceleration[2] = sensorEvent.values[2] - gravity[2]
+
+
+                minA=min(minA,abs(linearAcceleration.sum()))
+                if ((abs(linearAcceleration.sum()) < 0.5)) {
+
+                    print("works")
+
+                    if (!(mediaPlayer.isPlaying)){
+                        mediaPlayer.start()
+                    }
+                }
+                println(minA.toString())
+            }
+            Sensor.TYPE_LIGHT->{
+                if(lightLastChange==null){
+                    lightLastChange= true
+                }
+                else if ((!(lightLastChange as Boolean))&&sensorEvent.values[0]>70){
+                    lightLastChange=true
+                    if (!tts.isSpeaking){
+                        tts.speak(getString(R.string.hi), TextToSpeech.QUEUE_ADD,null,"Hi")
+                    }
+                }
+                else if (sensorEvent.values[0]<70 && lightLastChange as Boolean){
+                    lightLastChange=false
+                }
+
+
+            }
+        }
+    }
+
+
+}
--- app/src/main/java/com/why/lab7/MainActivity.kt
+++ app/src/main/java/com/why/lab7/MainActivity.kt
@@ -1,104 +1,54 @@
 package com.why.lab7
 
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.util.Log
+import android.os.IBinder
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeech.*
+import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
-import org.w3c.dom.Text
-import kotlin.math.absoluteValue
-import kotlin.math.pow
-import kotlin.math.sqrt
+import java.util.*
 
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
-    private var mSensorManager : SensorManager ?= null
-
-    private var mAccelerometer : Sensor ?= null
+class MainActivity : AppCompatActivity() {
+    var myService: FallService? = null
+    var isBound = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        mAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-
+        val intent = Intent(this, FallService::class.java)
+        bindService(intent, myConnection, Context.BIND_AUTO_CREATE)
     }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-
-    }
-
-    override fun onSensorChanged(sensorEvent: SensorEvent) {
-
-        val mySensor: Sensor = sensorEvent.sensor
-        when (mySensor.type) {
-            Sensor.TYPE_ACCELEROMETER -> {
-                val event=sensorEvent
-                mAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-
-                val alpha = 0.8
-
-                var gravity= mutableListOf<Double>(0.0,0.0,0.0)
-                gravity[0] = alpha * gravity[0] + (1 - alpha)
-                gravity[0]=gravity[0]* event.values[0]
-                gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1]
-                gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2]
-
-
-                var linearAcceleration=mutableListOf<Double>(0.0,0.0,0.0)
-                linearAcceleration[0] = event.values[0] - gravity[0]
-                linearAcceleration[1] = event.values[1] - gravity[1]
-                linearAcceleration[2] = event.values[2] - gravity[2]
-
-
-
-                if (((linearAcceleration.sum()) < 1 &&linearAcceleration.sum()>0)||(linearAcceleration.sum()>(-1)&&linearAcceleration.sum()<0)) {
-
-                    print("works")
-                    val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.scream)
-                    mediaPlayer?.start()
-                    findViewById<TextView>(R.id.hel).text = "ahhhh"
-                }else{
-                    findViewById<TextView>(R.id.hel).text = "Hello World!"
-
-                }
-            }
-            Sensor.TYPE_GYROSCOPE->{
-
-
-
-
-                if (sensorEvent.values.take(3).sum() < 3) {
-
-                    val mediaPlayer: MediaPlayer? = MediaPlayer.create(this, R.raw.scream)
-                    mediaPlayer?.start()
-
-                }
-
-            }
+    private val myConnection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName,
+                                        service: IBinder
+        ) {
+            val binder = service as FallService.LocalBinder
+            myService = binder.getService()
+            isBound = true
         }
 
-
-
-    }
-    protected override fun onResume() {
-        super.onResume();
-        mSensorManager?.registerListener(this, this.mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        // repeat that line for each sensor you want to monitor
+        override fun onServiceDisconnected(name: ComponentName) {
+            isBound = false
+        }
     }
 
 
-    protected override fun onPause() {
-        super.onPause();
-        mSensorManager?.unregisterListener(this);
-    }
+
+
+
+
+
 
 
 
